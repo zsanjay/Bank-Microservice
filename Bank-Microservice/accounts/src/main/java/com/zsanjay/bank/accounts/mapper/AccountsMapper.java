7c9ec6308ae14dae69998fe51af3dee94a57f843
Sanjay Mehta <sanjayofficial94@gmail.com>
2025-12-02 18:12:30 +0530

    Microservices Dockerized

--- Bank-Microservice/accounts/src/main/java/com/zsanjay/bank/accounts/mapper/AccountsMapper.java
+++ Bank-Microservice/accounts/src/main/java/com/zsanjay/bank/accounts/mapper/AccountsMapper.java
@@ -3,10 +3,17 @@ package com.zsanjay.bank.accounts.mapper;
 import com.zsanjay.bank.accounts.dto.AccountsDto;
 import com.zsanjay.bank.accounts.entity.Accounts;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountsMapper {
 
     AccountsDto toDto(Accounts accounts);
+
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "customer", ignore = true)
     Accounts toEntity(AccountsDto accountsDto);
 }
--- Bank-Microservice/accounts/src/main/java/com/zsanjay/bank/accounts/mapper/CustomerMapper.java
+++ Bank-Microservice/accounts/src/main/java/com/zsanjay/bank/accounts/mapper/CustomerMapper.java
@@ -3,10 +3,18 @@ package com.zsanjay.bank.accounts.mapper;
 import com.zsanjay.bank.accounts.dto.CustomerDto;
 import com.zsanjay.bank.accounts.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
+    @Mapping(target = "accounts", ignore = true)
     CustomerDto toDto(Customer customer);
+
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "customerId", ignore = true)
     Customer toEntity(CustomerDto customerDto);
 }
